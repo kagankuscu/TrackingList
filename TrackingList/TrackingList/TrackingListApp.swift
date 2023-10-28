3d4d634692a01f8693c5373c301e375e4235641a
kagankuscu <kuscukagan@gmail.com>
2023-10-28 20:03:34 +0300

    Project Created.

---
+++ TrackingList/TrackingList/TrackingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingListApp.swift
+//  TrackingList
+//
+//  Created by Kagan Kuscu on 28.10.23.
+//
+
+import SwiftUI
+
+@main
+struct TrackingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
