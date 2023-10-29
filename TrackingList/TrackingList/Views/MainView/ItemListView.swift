ce20eaf5b88f9012b9fb16fe151b46aabcf4ce1d
kagankuscu <kuscukagan@gmail.com>
2023-10-29 19:07:03 +0200

    ItemListView created.

---
+++ TrackingList/TrackingList/Views/MainView/ItemListView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemListView.swift
+//  TrackingList
+//
+//  Created by Kagan Kuscu on 29.10.23.
+//
+
+import SwiftUI
+
+struct ItemListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemListView()
+}
